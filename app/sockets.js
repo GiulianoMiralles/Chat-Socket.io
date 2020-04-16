2f75c92fdfabc34ffedaefaadf376c17a9bfb859
G-Miralles <g.miralles@itecriocuarto.org.ar>
2020-04-16 00:27:53 -0300

    envio de mensajes realizo

--- app/sockets.js
+++ app/sockets.js
@@ -3,6 +3,8 @@ module.exports = (io) => {
     io.on('connection', (socket) => {
         console.log('Nuevo usuario conectado')
         addUser(socket);
+        disconnectUser(socket);
+        newMessge(socket);
     });
 }
 
@@ -23,4 +25,21 @@ function updateUsers(socket) { //actualiza y muetra mi lista de usuarios conecta
     socket.emit('updateUsers', { users }) //mando la lista de ususarios conectados a mi cliente
     socket.broadcast.emit('updateUsers', { users }) //Esto hace a que todos los usuarios se les actulice
 
+}
+
+function disconnectUser(socket) {
+    socket.on('disconnect', function() {
+        if (socket.username) {
+            users.splice(users.indexOf(socket.username), 1); //Busco el ususario que se desconecto y lo saco de la lista
+        }
+        updateUsers(socket); // una vez que saque al usuario de la lista la actualizo
+    })
+}
+
+
+function newMessage(socket) { //tomo el evento envio de mensajes de chat.js (newMessage)
+    socket.on('newMessage', function(data) {
+        socket.emit('updateMessages', data); //actualizo los mensajes en mi cliente
+        socket.broadcast.emit('updateMessages', data); //actualizo el mensaje que yo mande en los clientes del resto
+    })
 }
--- public/scripts/chat.js
+++ public/scripts/chat.js
@@ -2,6 +2,9 @@ $(document).ready(function() {
     let socket = io('http://localhost:3000');
     username(socket);
     updateUsers(socket);
+    newMessage(socket);
+    updateMessage(socket);
+
 });
 //creo la funcion username en mi cliente y le paso el socket como parametro 
 function username(socket) {
@@ -23,5 +26,46 @@ function updateUsers(socket) { //Aqui escucho el evento que me manda el servidor
             html += '</div';
             $('#users').append(html); //concateno el usuario que se conecto con el resto
         }
+    });
+}
+
+function newMessge(socket) { //Envio de mensajes
+    $('#message').keydown(function(ev) { //el mensaje se envia al tocar la tecla enter
+        if (ev.keycode == 13) { // la tecla enter en la numero 13
+            ev.preventDefault(); // evito enviar el mensaje en blanco
+            $('#send-msg-form').submit();
+            document.querySelector('#send-msg-form').reset(); //El input se vacia cuando mando un mensaje
+        }
+    })
+    $('#send-msg-form').submit(function(ev) { //aqui evito que mande mensajes en blanco
+        ev.preventDefault();
+        socket.emit('newMessage'), { //Aqui tomo el valor del input junto con el nombre y el genero que estan en la localStorage
+            username: localStorage.username,
+            genero: localStorage.genero,
+            message: $('#message').val()
+        }
+
+    })
+
+}
+
+
+function updateMessages(socket) {
+    socket.on('updateMessages', function(data) {
+        let html = '';
+        if (data.username == localStorage.username) { //aqui verifico si el ususario es el mismo que tengo en la LocalStoarage quiere decir que soy yo, entonces los mensajes que mande se van a ver del lado derecho
+            html += '<div class="my-msg full-width flex">';
+            if (data.genero == 'Male') html += '<div class="my-style-m message"><h4> Tú </h4>';
+            else html += '<div class="my-style-f message"><h4> Tú </h4>';
+            html += '<p class="lighter">' + data.message + '</p>';
+            html += '</div></div>';
+        } else { //Por el contrario los mensajes que mande el resto se van a ver del lado izquierdo
+            html += '<div class="full-width flex">';
+            if (data.genero == 'Male') html += '<div class="blue message"><h4>' + data.username + '</h4>';
+            else html += '<div class="pink message"><h4>' + data.username + '</h4>';
+            html += '<p class="lighter">' + data.message + '</p>';
+            html += '</div></div>';
+        }
+        $('#msg-list').append(html);
     });
 }
